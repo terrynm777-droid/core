a95717836f880c1804c92bddc11ffbecd9d015f6
Terry <terrynm777@gmail.com>
2026-02-26 17:11:20 +0900

    update favicon to v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,17 @@ import { Analytics } from "@vercel/analytics/react";
 export const metadata: Metadata = {
   title: "Core",
   description: "Core",
+  icons: {
+    icon: [{ url: "/favicon-v2.ico", type: "image/x-icon" }],
+    shortcut: [{ url: "/favicon-v2.ico", type: "image/x-icon" }],
+  },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
